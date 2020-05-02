d6c57d71eaa1d98b866517be2d7a4b4cdc3e46f7
Roland Doda <rolanddoda2014@gmail.com>
2020-05-02 20:34:31 +0200

    extend `package.json` + render templates

--- src/install.js
+++ src/install.js
@@ -1,10 +1,17 @@
-/**
- * Quasar App Extension install script
- *
- * Docs: https://quasar.dev/app-extensions/development-guide/install-api
- * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
- */
-
 module.exports = function (api) {
-  //
+  api.compatibleWith('quasar', '>=1.0.0')
+  api.compatibleWith('@quasar/app', '>=1.0.0')
+  
+  api.extendPackageJson({
+    scripts: {
+      "gh-pages-deploy": "node scripts/gh-pages-deploy.js"
+    },
+    "devDependencies": {
+      "chalk": "^4.0.0",
+      "execa": "^4.0.0",
+      "node-emoji": "^1.10.0"
+    }
+  })
+  
+  api.render('./templates')
 }
