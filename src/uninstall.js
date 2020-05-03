fef886581bba2be6fc174e98beb42b36f42c2512
Roland Doda <rolanddoda2014@gmail.com>
2020-05-03 17:35:30 +0200

    Add uninstall script

--- src/uninstall.js
+++ src/uninstall.js
@@ -1,10 +1,11 @@
-/**
- * Quasar App Extension uninstall script
- *
- * Docs: https://quasar.dev/app-extensions/development-guide/uninstall-api
- * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/UninstallAPI.js
- */
+const fs = require('fs')
 
 module.exports = function (api) {
-  //
+  const ghActionPath = api.resolve.app('.github/workflows/gh-pages-deploy.yml')
+  const ghPagesScriptPath = api.resolve.app('scripts/gh-pages-deploy.js')
+  
+  if (fs.existsSync(ghActionPath)) api.removePath(ghActionPath)
+  if (fs.existsSync(ghPagesScriptPath)) api.removePath(ghPagesScriptPath)
+  
+  api.onExitLog(`Thanks for using this extension.`)
 }
