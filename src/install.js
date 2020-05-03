b71fe97ca03b8cf1dc051f548578ec5f1ddd9311
Roland Doda <rolanddoda2014@gmail.com>
2020-05-03 16:46:04 +0200

    Add exit log

--- src/install.js
+++ src/install.js
@@ -7,4 +7,10 @@ module.exports = async function (api) {
   plugin.extendPackageJSON(api)
   await plugin.addPublicPathToConfig(api)
   await plugin.addTemplateFiles(api)
+  api.onExitLog(`
+      🧾🧾 Now you only have to commit and push 🧾🧾
+      🔥🔥 Enjoy automatic deployment 🔥🔥
+      🌟🌟 Please don't forget to star the project on Github if you like this extension. 🌟🌟
+      Project URL: https://github.com/Rolanddoda/quasar-ext-gh-pages-auto-deploy
+    `)
 }
